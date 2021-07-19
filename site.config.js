1633377897dfb65165eff16956d2f807fda5961a
sine90s <57471454+sine90s@users.noreply.github.com>
2021-07-19 15:15:22 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -18,7 +18,7 @@ module.exports = {
 
   // social usernames (optional)
   twitter: 'transitive_bs',
-  github: 'mascot0204',
+  github: 'sine90s',
   linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
